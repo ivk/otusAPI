e906229144bc419597924691becc792c7702d883
ivk <i_smith@mail.ru>
2024-05-12 22:06:33 +0300

    pep recommends

--- tests/test_ya.py
+++ tests/test_ya.py
@@ -5,5 +5,3 @@ def test_address(status_code, base_url):
     headers = {"Content-Type": "application/json"}
     response = requests.get(url=base_url, headers=headers)
     assert response.status_code == int(status_code)
-
-
